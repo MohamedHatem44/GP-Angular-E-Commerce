a647907e48f5be8918fe08b2b1335bacc931ae90
Mohamed Hatem <mohamed.hatem157@example.com>
2024-05-31 18:59:40 +0300

    Hatem - configure routing and add login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+/*--------------------------------------------------------------------------------------------------------------------*/
+const routes: Routes = [
+  { path: 'users/login', component: LoginComponent },
+  { path: 'users/register', component: RegisterComponent },
+];
+/*--------------------------------------------------------------------------------------------------------------------*/
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+/*--------------------------------------------------------------------------------------------------------------------*/
+export class AppRoutingModule {}
+/*--------------------------------------------------------------------------------------------------------------------*/
