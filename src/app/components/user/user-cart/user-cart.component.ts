2ca024d72677ca3146c35073080fe12004e9b0e3
Mohamed Hatem <mohamed.hatem157@example.com>
2024-06-23 13:19:36 +0300

    load user cart and edit quantity

--- src/app/components/user/user-cart/user-cart.component.ts
+++ src/app/components/user/user-cart/user-cart.component.ts
@@ -1,10 +1,108 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../../../services/cart.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { ImgModalComponent } from '../../modals/img-modal/img-modal.component';
+/*--------------------------------------------------------------------*/
 @Component({
   selector: 'app-user-cart',
   templateUrl: './user-cart.component.html',
-  styleUrl: './user-cart.component.css'
+  styleUrl: './user-cart.component.css',
 })
-export class UserCartComponent {
+/*--------------------------------------------------------------------*/
+export class UserCartComponent implements OnInit {
+  // Component properties
+  cartLoading: boolean = false;
+  brandIdToDelete: number;
+  shoppingCart: any;
+  cartItems: any[];
+  // brands: (Brand & { deleting?: boolean })[] = [];
+  apiError: string | null = null;
+  noItems: boolean = false;
+  searchInput: string = '';
+  /*-----------------------------------------------------------------*/
+  // Ctor
+  constructor(private _CartService: CartService, private _ModalService: NgbModal, private _ToastrService: ToastrService) {}
+  /*-----------------------------------------------------------------*/
+  // Ng OnInit
+  ngOnInit(): void {
+    this.getUserCart();
+  }
+  /*-----------------------------------------------------------------*/
+  getUserCart() {
+    this.cartLoading = true;
+    this.apiError = null;
+    this._CartService.getShoppingCartByUserFromClaims().subscribe({
+      next: (response: any) => {
+        this.shoppingCart = response;
+        console.log(this.shoppingCart);
 
+        this.cartItems = this.shoppingCart.cartItems;
+        console.log(this.cartItems);
+
+        this.cartLoading = false;
+        // this.noItems = this.brands.length === 0;
+      },
+      error: (err) => {
+        this.apiError = 'Failed to load Cart, Please try again.';
+        this._ToastrService.error('Failed to load Cart, Please try again.');
+        this.cartLoading = false;
+      },
+    });
+  }
+  /*-----------------------------------------------------------------*/
+  // Open Img Modal
+  openImgModal(item: any): void {
+    const modalRef = this._ModalService.open(ImgModalComponent);
+    modalRef.componentInstance.model = item;
+  }
+  /*-----------------------------------------------------------------*/
+  decreaseQuantity(item: any) {
+    if (item.quantity > 1) {
+      item.quantity--;
+      this.updateItemQuantity(item);
+    }
+  }
+  /*-----------------------------------------------------------------*/
+  increaseQuantity(item: any) {
+    const maxQuantity = 20;
+    if (item.quantity >= maxQuantity) {
+      this._ToastrService.error(`You can only add up to ${maxQuantity} items.`);
+      return;
+    }
+    item.quantity++;
+    this.updateItemQuantity(item);
+  }
+  /*-----------------------------------------------------------------*/
+  updateItemQuantity(item: any) {
+    const dto = { itemId: item.id, quantity: item.quantity };
+    this._CartService.updateItemQuantity(dto).subscribe({
+      next: (response: any) => {
+        const index = this.cartItems.findIndex((i) => i.id === item.id);
+        if (index !== -1) {
+          this.cartItems[index].totalItemPrice = item.quantity * item.itemPrice;
+        }
+        this.shoppingCart.itemsCount = this.calculateTotalItemsCount();
+        this.shoppingCart.totalCartPrice = this.calculateTotalCartPrice();
+      },
+      error: (error) => {
+        if (error.status === 400) {
+          item.quantity--;
+          this._ToastrService.error(`This Product does not have enough quantity in stock, Only available ${item.quantity}`);
+          return;
+        } else {
+          this._ToastrService.error('Failed to update quantity');
+        }
+      },
+    });
+  }
+  /*-----------------------------------------------------------------*/
+  private calculateTotalItemsCount(): number {
+    return this.cartItems.reduce((acc, item) => acc + item.quantity, 0);
+  }
+  /*-----------------------------------------------------------------*/
+  private calculateTotalCartPrice(): number {
+    return this.cartItems.reduce((acc, item) => acc + item.quantity * item.itemPrice, 0);
+  }
+  /*-----------------------------------------------------------------*/
 }
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -9,15 +9,26 @@ import { Cart } from '../models/cart';
 /*--------------------------------------------------------------------*/
 export class CartService {
   baseUrl = 'http://localhost:5185/api/ShoppingCarts';
-  // http://localhost:5185/api/ShoppingCarts/AddToCart
   /*------------------------------------------------------------------*/
   // Ctor
   constructor(private _HttpClient: HttpClient) {}
+  /*------------------------------------------------------------------*/
+  // Get a Specific Shopping Cart By User Claims
+  // Get: api/ShoppingCarts/UserShoppingCart
+  getShoppingCartByUserFromClaims(): Observable<any> {
+    return this._HttpClient.get<any>(`${this.baseUrl}/UserShoppingCart`).pipe(delay(0));
+  }
   /*------------------------------------------------------------------*/
   // Add Items To Shopping Cart
   // Post: api/ShoppingCarts/AddToCart
   addToCart(item: Cart): Observable<Cart> {
     return this._HttpClient.post<Cart>(`${this.baseUrl}/AddToCart`, item).pipe(delay(3000));
+  }
+  /*------------------------------------------------------------------*/
+  // Edit Item Quantity in Shopping Cart
+  // Patch: api/ShoppingCarts
+  updateItemQuantity(dto: any): Observable<any> {
+    return this._HttpClient.patch<any>(`${this.baseUrl}`, dto);
   }
   /*------------------------------------------------------------------*/
 }
