bc3fd38d25d00c5b4f7e6155b384a0f399a667a4
Mohamed Hatem <mohamed.hatem157@example.com>
2024-06-27 06:21:12 +0300

    &feat: Order Service

---
+++ src/app/services/order.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, delay, tap } from 'rxjs';
+import { CartService } from './cart.service';
+/*--------------------------------------------------------------------*/
+@Injectable({
+  providedIn: 'root',
+})
+/*--------------------------------------------------------------------*/
+export class OrderService {
+  baseUrl = 'http://localhost:5185/api/Orders';
+  /*------------------------------------------------------------------*/
+  // Ctor
+  constructor(private _HttpClient: HttpClient, private _CartService: CartService) {}
+  /*------------------------------------------------------------------*/
+  // Create Order From Cart
+  // Post: api/orders/CreateOrderFromCart
+  createOrder(): Observable<any> {
+    return this._HttpClient.post<any>(`${this.baseUrl}/CreateOrderFromCart`, {}).pipe(
+      delay(3000),
+      tap(() => {
+        this._CartService.updateCartItemCount();
+      })
+    );
+  }
+  /*------------------------------------------------------------------*/
+}
