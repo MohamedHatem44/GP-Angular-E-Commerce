4fe7e4be1178103196d4f4ff93889c8d81d4f996
Mohamed Hatem <mohamed.hatem157@example.com>
2024-06-28 03:50:09 +0300

    feat: Implement Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,6 +35,7 @@ import { AdminProductDetailsComponent } from './components/admin/admin-product-d
 import { AdminBlogsComponent } from './components/admin/admin-blogs/admin-blogs.component';
 import { AdminBlogFormComponent } from './components/admin/admin-blog-form/admin-blog-form.component';
 import { AdminProductReviewsComponent } from './components/admin/admin-product-reviews/admin-product-reviews.component';
+import { authGuard } from './guard/auth.guard';
 /*-----------------------------------------------------------------------------------------------------------------*/
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
@@ -42,6 +43,7 @@ const routes: Routes = [
   {
     path: 'admindashboard',
     component: AdminDashboardComponent,
+    canActivate: [authGuard],
     children: [
       { path: '', component: AdminMainComponent, pathMatch: 'full' },
       { path: 'products', component: AdminProductsComponent },
